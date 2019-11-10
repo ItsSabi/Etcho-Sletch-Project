e4aea142ce090c5824f88ed279c9a377502146b9
itssabi <sabi.sabi@gmail.com>
2019-11-10 15:45:05 -0600

    Reworking eveverything.
    >

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-const divContainer = document.getElementById('container');
+const container = document.getElementById("container");
 
-let cell = document.createElement('div');
+function makeRows(rows, cols) {
+  container.style.setProperty(1, rows);
+  container.style.setProperty(1, cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
 
+makeRows(16,16);

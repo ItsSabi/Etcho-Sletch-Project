e29bb04572ccde5c6f5892fef50e2cdaf9523b58
itssabi <sabi.sabi@gmail.com>
2019-11-10 23:07:42 -0600

    Got the grid to work properly with the help of Stackoverflow.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 
 
 const container = document.getElementById('container');
-
-let rows ;
+let row;
 let cols;
 
 function gridMaker( rows, cols){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
 
-    for( i= 0; i <(rows * cols); i ++){
+    for( i= 0; i <(rows * cols); i++){
+       
+
         let cell = document.createElement('div');
         cell.innerText= (i+1);
         container.appendChild(cell).className ='grid-item';

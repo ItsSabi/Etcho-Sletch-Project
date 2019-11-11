7406b1e824fa45ad4d4b4e2a8fd12570359c6107
itssabi <sabi.sabi@gmail.com>
2019-11-10 22:12:19 -0600

    Got something working now!

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
-const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
-  container.style.setProperty(1, rows);
-  container.style.setProperty(1, cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
+
+const container = document.getElementById('container');
+
+let rows ;
+let cols;
+
+function gridMaker( rows, cols){
+
+    for( i= 0; i <(rows * cols); i ++){
+        let cell = document.createElement('div');
+        cell.innerText= (i+1);
+        container.appendChild(cell).className ='grid-item';
+    };
+
+
 };
 
-makeRows(16,16);
+gridMaker(16,16);
